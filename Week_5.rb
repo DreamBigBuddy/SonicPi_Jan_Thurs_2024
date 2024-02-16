3f43752da72090ba94effa0eb372d92dd3e45415
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2024-02-15 20:07:42 -0500

    Add files via upload

---
+++ Week_5.rb
@@ -0,0 +1,38 @@
+play :C4
+sleep 1
+
+play :C4
+sleep 1
+
+play :G4
+sleep 1
+
+play :G4
+sleep 1
+
+play :A4, sustain: 0.5
+sleep 0.5
+
+play :B4, sustain: 0.5
+sleep 0.5
+
+play :C5, sustain: 0.5
+sleep 0.5
+
+play :A4, sustain: 0.5
+sleep 0.5
+
+play :G4, sustain: 2
+sleep 2
+
+play :F4
+sleep 1
+
+play :F4
+sleep 1
+
+play :E4
+sleep 1
+
+play :E4
+sleep 1
